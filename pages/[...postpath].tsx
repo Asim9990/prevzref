1760fbd5e59937f63a806a11c6100fcc7df08fc6
Asim9990 <133738091+Asim9990@users.noreply.github.com>
2023-05-20 03:50:29 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -14,7 +14,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
 	// redirect if facebook is the referer or request contains fbclid
 	if (referringURL?.includes('facebook.com') || fbclid) {
-		return {
+		return { 
 			redirect: {
 				permanent: false,
 				destination: `${
